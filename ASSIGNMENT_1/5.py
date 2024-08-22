509e2f1eda8d2b2ef8019149b03c70420215ee7c
SAILAJ_SHYAM_MCA_SECA_2024-26 <139752636+iamsailajshyam@users.noreply.github.com>
2024-08-22 11:02:11 +0530

    Create 5.py

---
+++ ASSIGNMENT_1/5.py
@@ -0,0 +1,13 @@
+# Get two numbers from the user
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Swap the values of a and b
+temp = a
+a = b
+b = temp
+
+# Print the swapped values
+print("After swapping, the numbers are:")
+print("Now the first number =", a)
+print("Now the second number =", b)
